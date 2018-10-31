107059b0e8d4608328cc2eaa4528c940958f1272
JCesana <joshuacesana@gmail.com>
2018-10-31 14:16:01 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,30 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// const code = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a']
+
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    let key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
+      console.log(e);
+      index++;
+
+      if (index === code.length) {
+        alert("Konami Code enabled!");
+
+        index = 0;
+      }
+    } else {
+      console.log(e);
+      index = 0;
+    }
+
+  });
 }
+
+init();
